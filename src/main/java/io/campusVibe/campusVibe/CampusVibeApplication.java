7adb1fa6bbbdab43b2b0f6b9f9c643b4f6ecb12f
Deb-bie <asamoahjaeldeborah@gmail.com>
2024-09-02 17:37:47 +0000

    Initial commit

---
+++ src/main/java/io/campusVibe/campusVibe/CampusVibeApplication.java
@@ -0,0 +1,13 @@
+package io.campusVibe.campusVibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusVibeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusVibeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/campusVibe/campusVibe/CampusVibeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.campusVibe.campusVibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusVibeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
